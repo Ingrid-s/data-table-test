c29c90d8ee0c6a2e3095175207095300371b25fa
Ingrid Sagrero <starlling@hotmail.com>
2019-10-13 20:23:10 -0500

    pequeño refactor, quitar var, mostrar 2 decimales

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
-var data = require ('./brandDateData.json');
-var stores = require ('./assignedStore.json');
+const data = require ('./brandDateData.json');
+const stores = require ('./assignedStore.json');
 
 class Table extends Component{
   constructor() {
@@ -15,12 +15,11 @@ class Table extends Component{
 
   //Recorriendo stores para imprimir datos
  renderStoreData() {
-    return this.state.stores.map((store, index) => {
-       const { id, name, identifier } = store //destructuring
+    return this.state.stores.map((store) => {
+       const { name, identifier } = store //destructuring
      
        return (
-       
-          <tr key={id}>
+          <tr key={identifier}>
              <td>{name}</td>
           </tr>
        )
@@ -28,7 +27,7 @@ class Table extends Component{
  }
 
   renderPeasantsData() {
-    return this.state.data.map((data, index) => {
+    return this.state.data.map((data) => {
        const { identifier, peasants } = data //destructuring
        
        let suma = Object.values(peasants).reduce((a,b)=> a + b);
@@ -42,11 +41,11 @@ class Table extends Component{
  }
 
  renderVisitorsData() {
-    return this.state.data.map((data, index) => {
-       const { id, visitors } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, visitors } = data //destructuring
        let suma = Object.values(visitors).reduce((a,b)=> a + b);
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{suma}</td>
           </tr>
        )
@@ -54,25 +53,25 @@ class Table extends Component{
  }
 
  renderAttractionData() {
-    return this.state.data.map((data, index) => {
-       const { id, peasants, visitors } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, peasants, visitors } = data //destructuring
        let suma = Object.values(visitors).reduce((a,b)=> a + b);
       let suma2 = Object.values(peasants).reduce((a,b)=> a + b);
       let promedio = suma/suma2
        return (
-          <tr key={id}>
-               <td>{promedio}</td>
+          <tr key={identifier}>
+               <td>{promedio.toFixed(2)}</td>
           </tr>
        )
     })
  }
 
  renderCabinetData() {
-    return this.state.data.map((data, index) => {
-       const { id, cabinet } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, cabinet } = data //destructuring
        let suma = Object.values(cabinet).reduce((a,b)=> a + b);
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{suma}</td>
           </tr>
        )
@@ -80,11 +79,11 @@ class Table extends Component{
  }
 
  renderTicketsData() {
-    return this.state.data.map((data, index) => {
-       const { id, tickets } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, tickets } = data //destructuring
        let suma = Object.values(tickets).reduce((a,b)=> a + b);
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{suma}</td>
           </tr>
        )
@@ -92,40 +91,40 @@ class Table extends Component{
  }
 
   renderPersuasionData() {
-    return this.state.data.map((data, index) => {
-       const { id, tickets, visitors } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, tickets, visitors } = data //destructuring
        let suma = Object.values(tickets).reduce((a,b)=> a + b);
        let suma2 = Object.values(visitors).reduce((a,b)=> a + b);
         let promedio = suma/suma2
        return (
-          <tr key={id}>
-               <td>{promedio}</td>
+          <tr key={identifier}>
+               <td>{promedio.toFixed(2)}</td>
           </tr>
        )
     })
  }
 
  renderRevenueData() {
-    return this.state.data.map((data, index) => {
-       const { id, revenue } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, revenue } = data //destructuring
        let suma = Object.values(revenue).reduce((a,b)=> a + b);
      
        return (
-          <tr key={id}>
-               <td>{suma}</td>
+          <tr key={identifier}>
+               <td>{suma.toFixed(2)}</td>
           </tr>
        )
     })
  }
 
 renderAverageTicketData() {
-    return this.state.data.map((data, index) => {
-       const { id, revenue, tickets } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, revenue, tickets } = data //destructuring
        let suma = Object.values(revenue).reduce((a,b)=> a + b);
        let suma2 = Object.values(tickets).reduce((a,b)=> a + b);
-        let promedio = suma/suma2
+        let promedio = suma/suma2;
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{promedio}</td>
           </tr>
        )
@@ -133,12 +132,12 @@ renderAverageTicketData() {
  }
 
   renderItemsData() {
-    return this.state.data.map((data, index) => {
-       const { id, items } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, items } = data //destructuring
        let suma = Object.values(items).reduce((a,b)=> a + b);
      
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{suma}</td>
           </tr>
        )
@@ -146,13 +145,13 @@ renderAverageTicketData() {
  }
 
  renderItemperTicketData() {
-    return this.state.data.map((data, index) => {
-       const { id, items, tickets } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, items, tickets } = data //destructuring
        let suma = Object.values(items).reduce((a,b)=> a + b);
        let suma2 = Object.values(tickets).reduce((a,b)=> a + b);
-        let promedio = suma/suma2
+        let promedio = ((suma/suma2)/100).toPrecision(2);
        return (
-          <tr key={id}>
+          <tr key={identifier}>
                <td>{promedio}</td>
           </tr>
        )
@@ -160,14 +159,14 @@ renderAverageTicketData() {
  }
 
  renderAveragePermanenceData() {
-    return this.state.data.map((data, index) => {
-       const { id, permanence, permanenceCount } = data //destructuring
+    return this.state.data.map((data) => {
+       const { identifier, permanence, permanenceCount } = data //destructuring
        let suma = Object.values(permanence).reduce((a,b)=> a + b);
        let suma2 = Object.values(permanenceCount).reduce((a,b)=> a + b);
         let total = (((suma * 100) / suma2) / 6000000)
        return (
-          <tr key={id}>
-               <td>{total}</td>
+          <tr key={identifier}>
+               <td>{total.toFixed(2)}</td>
           </tr>
        )
     })
